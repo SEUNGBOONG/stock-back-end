8151a5b2da9676b5894b0af41029e51a81ce72a1
승엽 <tmdduqflfl@naver.com>
2024-10-15 00:52:08 +0900

    Fix : token 서비스 로직 구현

--- investement/src/main/java/com/example/investment_api/common/token/TokenService.java
+++ investement/src/main/java/com/example/investment_api/common/token/TokenService.java
@@ -12,21 +12,27 @@ public class TokenService {
     @Value("${API_ACCESS_TOKEN}")
     private String accessToken;
 
-    @Value("${api.appkey}")
+    @Value("${API_APP_KEY}")
     private String appKey;
 
-    @Value("${api.appsecret}")
+    @Value("${API_APP_SECRET}")
     private String appSecret;
 
     private final RestTemplate restTemplate = new RestTemplate();
 
-    @Scheduled(fixedRate = 86400000)
+    public String getAccessToken() {
+        if (accessToken == null || accessToken.isEmpty()) {
+            renewAccessToken();
+        }
+        return accessToken;
+    }
+
     public void renewAccessToken() {
         String url = "https://openapi.koreainvestment.com:9443/oauth2/tokenP";
 
         String requestBody = String.format("{\"grant_type\":\"client_credentials\",\"appkey\":\"%s\",\"appsecret\":\"%s\"}", appKey, appSecret);
 
         this.accessToken = restTemplate.postForObject(url, requestBody, String.class);
-
     }
+
 }
