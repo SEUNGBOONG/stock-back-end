c175e2e45ab647ec23cb6390df918cca8183fa8c
Hheojiwon <0618amy@naver.com>
2024-10-15 22:48:26 +0900

    feat: 테스트코드

---
+++ investement/src/test/java/com/example/investment_api/virtual/MemberAccountTest.java
@@ -0,0 +1,81 @@
+package com.example.investment_api.virtual;
+
+import com.example.investment_api.virtual.account.MemberAccount;
+import com.example.investment_api.virtual.account.MemberAccountRepository;
+import com.example.investment_api.virtual.account.MemberAccountService;
+import com.example.investment_api.virtual.calculator.dto.StockCalculationDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+public class MemberAccountTest {
+    @Mock
+    private MemberAccountRepository accountRepository;
+
+    @InjectMocks
+    private MemberAccountService memberAccountService;
+
+    private MemberAccount testAccount;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        testAccount = new MemberAccount(1L, "TestStock", 1000, 10);
+    }
+
+    @Test
+    void DTO_생성_테스트() {
+        Long memberId = 1L;
+        String stockName = "TestStock";
+
+        when(accountRepository.findByMemberIdAndStockName(memberId, stockName))
+                .thenReturn(Optional.of(testAccount));
+
+        StockCalculationDTO result = memberAccountService.getStockCalculationDTOList(memberId, stockName);
+
+        assertNotNull(result);
+        assertEquals("TestStock", result.stockName());
+        assertEquals(1000, result.buyPrice());
+        assertEquals(10, result.stockCount());
+        assertEquals(0, result.currentPrice());
+    }
+
+    @Test
+    void 아이디와_주식명으로_찾기() {
+        Long memberId = 1L;
+        String stockName = "TestStock";
+
+        when(accountRepository.findByMemberIdAndStockName(memberId, stockName))
+                .thenReturn(Optional.of(testAccount));
+
+        MemberAccount result = memberAccountService.getMemberAccount(memberId, stockName);
+
+        assertNotNull(result);
+        assertEquals(memberId, result.getMemberId());
+        assertEquals(stockName, result.getStockName());
+    }
+
+    @Test
+    void 멤버_아이디로_주식_찾기() {
+        Long memberId = 1L;
+        List<MemberAccount> accounts = Arrays.asList(testAccount);
+
+        when(accountRepository.findByMemberId(memberId)).thenReturn(Optional.of(accounts));
+
+        List<MemberAccount> result = memberAccountService.getMemberAccounts(memberId);
+
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals("TestStock", result.get(0).getStockName());
+    }
+}
