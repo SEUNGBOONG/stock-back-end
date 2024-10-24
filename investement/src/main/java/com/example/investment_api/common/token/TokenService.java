1985cf2af3de723365a824b4edd8f94a498fc231
승엽 <tmdduqflfl@naver.com>
2024-10-24 20:33:05 +0900

    refactor: crossorigin 제거

--- investement/src/main/java/com/example/investment_api/common/token/TokenService.java
+++ investement/src/main/java/com/example/investment_api/common/token/TokenService.java
@@ -1,38 +1,31 @@
 package com.example.investment_api.common.token;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class TokenService {
 
-    @Value("${API_ACCESS_TOKEN}")
+    @Value("${api.access_token}")
     private String accessToken;
 
-    @Value("${API_APP_KEY}")
+    @Value("${api.appkey}")
     private String appKey;
 
-    @Value("${API_APP_SECRET}")
+    @Value("${api.appsecret}")
     private String appSecret;
 
-    private RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate = new RestTemplate();
 
-    public TokenService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
-
-    public String getAccessToken() {
-        if (accessToken == null || accessToken.isEmpty()) {
-            renewAccessToken();
-        }
-        return accessToken;
-    }
-
+    @Scheduled(fixedRate = 86400000)
     public void renewAccessToken() {
         String url = "https://openapi.koreainvestment.com:9443/oauth2/tokenP";
+
         String requestBody = String.format("{\"grant_type\":\"client_credentials\",\"appkey\":\"%s\",\"appsecret\":\"%s\"}", appKey, appSecret);
-        this.accessToken = restTemplate.postForObject(url, requestBody, String.class);
+
+        String newToken = restTemplate.postForObject(url, requestBody, String.class);
+        this.accessToken = newToken;
     }
 }
