4e6f5b45caefae90347f97fbe7f8259ad08f2113
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-11-05 17:35:00 +0900

    feat(member): 계좌 구현

---
+++ investement/src/main/java/com/example/investment_api/member/domain/member/MemberDeposit.java
@@ -0,0 +1,31 @@
+package com.example.investment_api.member.domain.member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class MemberDeposit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private int deposit;
+
+    public MemberDeposit(Long memberId, int deposit) {
+        this.memberId = memberId;
+        this.deposit = deposit;
+    }
+}
