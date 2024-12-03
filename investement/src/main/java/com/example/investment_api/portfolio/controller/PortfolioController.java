dd8d36655a225d6ed91c81782eae55b98760880c
승엽 <tmdduqflfl@naver.com>
2024-12-03 14:52:17 +0900

    Refactoring  : 포트폴리오의 데이터 3개 만들어서 보내기

--- investement/src/main/java/com/example/investment_api/portfolio/controller/PortfolioController.java
+++ investement/src/main/java/com/example/investment_api/portfolio/controller/PortfolioController.java
@@ -1,7 +1,6 @@
 package com.example.investment_api.portfolio.controller;
 
 import com.example.investment_api.global.annotation.Member;
-import com.example.investment_api.portfolio.controller.dto.PortfolioRequest;
 import com.example.investment_api.portfolio.controller.dto.PortfolioResponse;
 
 import com.example.investment_api.portfolio.service.PortfolioService;
@@ -9,10 +8,10 @@ import com.example.investment_api.portfolio.service.PortfolioService;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/portfolio")
@@ -25,8 +24,8 @@ public class PortfolioController {
     }
 
     @PostMapping("/recommend")
-    public ResponseEntity<PortfolioResponse> getRecommendedPortfolio(@Member Long memberId) {
-        PortfolioResponse portfolioResponse = portfolioService.getRecommendedPortfolio(memberId);
+    public ResponseEntity<List<PortfolioResponse>> getRecommendedPortfolio(@Member Long memberId) {
+        List<PortfolioResponse> portfolioResponse = portfolioService.getRecommendedPortfolio(memberId);
         return ResponseEntity.ok(portfolioResponse);
     }
 }
--- investement/src/main/java/com/example/investment_api/virtual/account/domain/MemberAccount.java
+++ investement/src/main/java/com/example/investment_api/virtual/account/domain/MemberAccount.java
@@ -44,5 +44,4 @@ public class MemberAccount {
         this.stockCount -= count;
     }
 
-
 }
