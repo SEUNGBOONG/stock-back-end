2525dbf4fab12c30a217cf2f0c3af3a244174ba0
Hheojiwon <0618amy@naver.com>
2025-01-03 01:18:24 +0900

    refactor: 주식 구매 오류 수정

--- investement/src/main/java/com/example/investment_api/virtual/account/service/MemberAccountService.java
+++ investement/src/main/java/com/example/investment_api/virtual/account/service/MemberAccountService.java
@@ -130,6 +130,7 @@ public class MemberAccountService {
             } else {
                 sellStockImmediately(memberId, stockName, quantity);
             }
+            order.setProcessed(true);
             stockOrderRepository.save(order);
         }
     }
