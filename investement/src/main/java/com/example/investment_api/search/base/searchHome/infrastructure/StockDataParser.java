12464da9ba7b9a612d7346e9286411b0fd4f58c7
승엽 <tmdduqflfl@naver.com>
2025-01-13 17:09:35 +0900

    Refact: 데이터 클린코드

--- investement/src/main/java/com/example/investment_api/search/base/searchHome/infrastructure/StockDataParser.java
+++ investement/src/main/java/com/example/investment_api/search/base/searchHome/infrastructure/StockDataParser.java
@@ -13,6 +13,15 @@ import java.util.List;
 
 @Component
 public class StockDataParser {
+    public static final String DATA_RANK = "data_rank";
+    public static final String STOCK_PRICE = "stck_prpr";
+    public static final String STOCK_NAME = "hts_kor_isnm";
+    public static final String PREVIOUS_CHANGE = "prdy_vrss";
+    public static final String PREVIOUS_CHAGER_RATE = "prdy_ctrt";
+    public static final String TRADING_VLOUME = "acml_vol";
+    public static final String MARKET_CAPITALIZAITON = "stck_avls";
+    public static final String OUTPUT = "output";
+
     private final ObjectMapper objectMapper;
 
     public StockDataParser(final ObjectMapper objectMapper) {
@@ -39,13 +48,13 @@ public class StockDataParser {
     private void buildDataList(List<StockDataDTO> marketCapitalizationDTOList, Iterator<JsonNode> elements) {
         while (elements.hasNext()) {
             JsonNode marketCapitalizationOutput = elements.next();
-            String rank = marketCapitalizationOutput.path("data_rank").asText();
-            String stockPrice = marketCapitalizationOutput.path("stck_prpr").asText();
-            String stockName = marketCapitalizationOutput.path("hts_kor_isnm").asText();
-            String prevChangePrice = marketCapitalizationOutput.path("prdy_vrss").asText();
-            String prevChangeRate = marketCapitalizationOutput.path("prdy_ctrt").asText();
-            String tradingVolume = marketCapitalizationOutput.path("acml_vol").asText();
-            String marketCapitalization = marketCapitalizationOutput.path("stck_avls").asText();
+            String rank = marketCapitalizationOutput.path(DATA_RANK).asText();
+            String stockPrice = marketCapitalizationOutput.path(STOCK_PRICE).asText();
+            String stockName = marketCapitalizationOutput.path(STOCK_NAME).asText();
+            String prevChangePrice = marketCapitalizationOutput.path(PREVIOUS_CHANGE).asText();
+            String prevChangeRate = marketCapitalizationOutput.path(PREVIOUS_CHAGER_RATE).asText();
+            String tradingVolume = marketCapitalizationOutput.path(TRADING_VLOUME).asText();
+            String marketCapitalization = marketCapitalizationOutput.path(MARKET_CAPITALIZAITON).asText();
 
             marketCapitalizationDTOList.add(new StockDataDTO(rank, stockName, stockPrice, prevChangePrice, prevChangeRate, marketCapitalization, tradingVolume));
         }
@@ -53,6 +62,6 @@ public class StockDataParser {
 
     private JsonNode getJsonNode(final String responseBody) throws JsonProcessingException {
         JsonNode rootNode = objectMapper.readTree(responseBody);
-        return rootNode.path("output");
+        return rootNode.path(OUTPUT);
     }
 }
