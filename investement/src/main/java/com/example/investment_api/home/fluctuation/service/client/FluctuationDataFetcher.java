4ed8e60e083ea9358bbc330e10da11d4e9640bdc
승엽 <tmdduqflfl@naver.com>
2025-01-13 17:01:34 +0900

    Refact: 상수화 진행

--- investement/src/main/java/com/example/investment_api/home/fluctuation/service/client/FluctuationDataFetcher.java
+++ investement/src/main/java/com/example/investment_api/home/fluctuation/service/client/FluctuationDataFetcher.java
@@ -14,6 +14,28 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class FluctuationDataFetcher {
 
+    private static final String TR_ID = "tr_id";
+    public static final String APPSECRET = "appsecret";
+    public static final String APPKEY = "appkey";
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String BEARER = "Bearer ";
+    public static final String CONTENT_TYPE = "Content-Type";
+    public static final String KOREAN_INVESTMENT_URL = "https://openapi.koreainvestment.com:9443/uapi/domestic-stock/v1/ranking/fluctuation?"
+            + "fid_cond_mrkt_div_code=J&"
+            + "fid_cond_scr_div_code=20170&"
+            + "fid_input_iscd=0000&"
+            + "fid_rank_sort_cls_code=0&"
+            + "fid_input_cnt_1=0&"
+            + "fid_prc_cls_code=0&"
+            + "fid_input_price_1=&"
+            + "fid_input_price_2=&"
+            + "fid_vol_cnt=&"
+            + "fid_trgt_cls_code=0&"
+            + "fid_trgt_exls_cls_code=0&"
+            + "fid_div_cls_code=0&"
+            + "fid_rsfl_rate1=&"
+            + "fid_rsfl_rate2=";
+
     private final RestTemplate restTemplate;
 
     @Value("${API_APP_KEY}")
@@ -46,28 +68,14 @@ public class FluctuationDataFetcher {
     }
 
     private void setHeader(final HttpHeaders headers) {
-        headers.set("tr_id", trId);
-        headers.set("appsecret", appSecret);
-        headers.set("appkey", appKey);
-        headers.set("Authorization", "Bearer " + accessToken);
-        headers.set("Content-Type", "application/json");
+        headers.set(TR_ID, trId);
+        headers.set(APPSECRET, appSecret);
+        headers.set(APPKEY, appKey);
+        headers.set(AUTHORIZATION, BEARER + accessToken);
+        headers.set(CONTENT_TYPE, "application/json");
     }
 
     private String setURL() {
-        return "https://openapi.koreainvestment.com:9443/uapi/domestic-stock/v1/ranking/fluctuation?"
-                + "fid_cond_mrkt_div_code=J&"
-                + "fid_cond_scr_div_code=20170&"
-                + "fid_input_iscd=0000&"
-                + "fid_rank_sort_cls_code=0&"
-                + "fid_input_cnt_1=0&"
-                + "fid_prc_cls_code=0&"
-                + "fid_input_price_1=&"
-                + "fid_input_price_2=&"
-                + "fid_vol_cnt=&"
-                + "fid_trgt_cls_code=0&"
-                + "fid_trgt_exls_cls_code=0&"
-                + "fid_div_cls_code=0&"
-                + "fid_rsfl_rate1=&"
-                + "fid_rsfl_rate2=";
+        return KOREAN_INVESTMENT_URL;
     }
 }
