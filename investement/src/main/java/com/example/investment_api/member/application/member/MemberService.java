25ef9c7098ec69d01da0283f2a6af166b06cfb66
승엽 <tmdduqflfl@naver.com>
2025-01-14 19:50:28 +0900

    Feat: service ->  dto 반환 추가

--- investement/src/main/java/com/example/investment_api/member/application/member/MemberService.java
+++ investement/src/main/java/com/example/investment_api/member/application/member/MemberService.java
@@ -2,8 +2,12 @@ package com.example.investment_api.member.application.member;
 
 import com.example.investment_api.member.domain.member.Member;
 import com.example.investment_api.member.infrastructure.member.MemberJpaRepository;
+import com.example.investment_api.member.ui.member.dto.MemberDto;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -22,4 +26,12 @@ public class MemberService {
                 .orElseThrow(RuntimeException::new);
         return member.getDeposit();
     }
+
+    public List<MemberDto> getTop5MembersByDeposit() {
+        List<Member> members = memberJpaRepository.findTop5ByReleaseCheckTrueOrderByDepositDesc();
+        return members.stream()
+                .map(member -> new MemberDto(member.getMemberNickName(), member.getDeposit()))
+                .toList();
+    }
+
 }
