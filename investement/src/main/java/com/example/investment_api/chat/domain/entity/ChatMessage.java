cf335ef8f248810f2aaa778c7502db995e90c540
승엽 <tmdduqflfl@naver.com>
2025-01-19 17:25:13 +0900

    Feat: 엔티티

--- investement/src/main/java/com/example/investment_api/chat/domain/entity/ChatMessage.java
+++ investement/src/main/java/com/example/investment_api/chat/domain/entity/ChatMessage.java
@@ -1,8 +1,6 @@
 package com.example.investment_api.chat.domain.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -10,36 +8,26 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@NoArgsConstructor
 public class ChatMessage {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "room_id", nullable = false)
+    @ManyToOne
+    @JoinColumn(name = "chat_room_id")
     private ChatRoom chatRoom;
 
-    @Column(nullable = false)
-    private String sender;
-
-    @Column(nullable = false)
+    private Long senderId;
     private String content;
 
-    @Column(nullable = false)
-    private LocalDateTime timestamp;
+    public ChatMessage() {}
 
-    public ChatMessage(ChatRoom chatRoom, String sender, String content) {
+    public ChatMessage(ChatRoom chatRoom, Long senderId, String content) {
         this.chatRoom = chatRoom;
-        this.sender = sender;
+        this.senderId = senderId;
         this.content = content;
-        this.timestamp = LocalDateTime.now();
     }
 }
