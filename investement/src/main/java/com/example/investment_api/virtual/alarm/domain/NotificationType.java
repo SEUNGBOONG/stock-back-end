8b446fd05153e44c97388289cbd1b514e7f39100
Hheojiwon <0618amy@naver.com>
2025-01-27 16:27:02 +0900

    feat: 알람 타입 추가

--- investement/src/main/java/com/example/investment_api/virtual/alarm/domain/NotificationType.java
+++
@@ -1,6 +0,0 @@
-package com.example.investment_api.virtual.alarm.domain;
-
-public enum NotificationType {
-    BUY_SUCCESS,
-    SELL_SUCCESS
-}
---
+++ investement/src/main/java/com/example/investment_api/virtual/notification/domain/NotificationType.java
@@ -0,0 +1,7 @@
+package com.example.investment_api.virtual.notification.domain;
+
+public enum NotificationType {
+    BUY_SUCCESS,
+    SELL_SUCCESS,
+    TOP3_NEWS
+}
